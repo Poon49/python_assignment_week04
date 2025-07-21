65839a6176f3e49b89ae83a6ce7f53891b75f23b
Poon <poonnachote@gmail.com>
2025-07-21 14:54:37 +0700

    d

---
+++ lambda_calculator.py
@@ -0,0 +1,9 @@
+addition = lambda x, y: x+y
+subtraction = lambda x, y: x-y
+multiplication = lambda x, y: x*y
+division= lambda x, y: x/y
+
+print(addition(10, 20))
+print(subtraction(10, 20))
+print(multiplication(10, 20))
+print(division(10, 20))
---
+++ string_operations.py
@@ -0,0 +1,11 @@
+def reverse_string(x):
+    sorted(x)
+
+def capitalize_string(x):
+    x.capitalize()
+
+def lowercase_string(x):
+    x.lower()
+
+def uppercase_string(x):
+    x.upper()
